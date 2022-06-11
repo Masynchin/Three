ff497703cffc25a2370ceb7375e2353653d88a51
Max Smirnov <masynchin@gmail.com>
2022-06-11 11:54:16 +0300

    Add Three (#4)

--- src/Main/GridRow.purs
+++ src/Main/GridRow.purs
@@ -1,6 +1,7 @@
 module Main.GridRow
-  ( GridRow(..)
+  ( GridRow
   , firstCell
+  , gridRow
   , rowWinner
   , secondCell
   , thirdCell
@@ -10,26 +11,31 @@ module Main.GridRow
 import Prelude
 
 import Data.Maybe (Maybe(..))
+import Data.Three (Three(..), fst, snd, trd)
 import Main.Cell (Cell, playerCell)
 import Main.Player (Player)
 
 -- | Grid row.
-data GridRow = GridRow Cell Cell Cell
+type GridRow = Three Cell
+
+-- | Grid row ctor.
+gridRow :: Cell -> Cell -> Cell -> GridRow
+gridRow = Three
 
 -- | Winner of row.
 rowWinner :: GridRow -> Maybe Player
-rowWinner (GridRow c1 c2 c3)
+rowWinner (Three c1 c2 c3)
   | (c1 == c2) && (c2 == c3) = playerCell c1
   | otherwise = Nothing
 
 -- | First (left) cell of row.
 firstCell :: GridRow -> Cell
-firstCell (GridRow c _ _) = c
+firstCell = fst
 
 -- | Second (middle) cell of row.
 secondCell :: GridRow -> Cell
-secondCell (GridRow _ c _) = c
+secondCell = snd
 
 -- | Third (right) cell of row.
 thirdCell :: GridRow -> Cell
-thirdCell (GridRow _ _ c) = c
+thirdCell = trd
