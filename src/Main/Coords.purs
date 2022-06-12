6dbbaca4f476b199e5ce5f5c74c1a4d82ddf37c8
Max Smirnov <masynchin@gmail.com>
2022-06-12 03:29:51 +0300

    Add cubeCell

---
+++ src/Main/Coords.purs
@@ -0,0 +1,32 @@
+module Main.Coords
+  ( Coord(..)
+  , Coords
+  , allCoords
+  , ofCoord
+  )
+  where
+
+import Prelude
+
+import Data.List (List(..), (:))
+import Data.Three (Three(..), fst, snd, trd)
+
+-- | Coord. Since cube is 3x3x3,
+-- | only three coord by each axis possible.
+data Coord = Zero | One | Two
+
+-- | Coords of cell in cube (X Y Z order).
+type Coords = Three Coord
+
+-- | All possible coords from Coord.
+allCoords :: List Coords
+allCoords = Three
+  <$> Zero : One : Two : Nil
+  <*> Zero : One : Two : Nil
+  <*> Zero : One : Two : Nil
+
+-- | Elem in Three with given Coord.
+ofCoord :: forall a. Coord -> (Three a -> a)
+ofCoord Zero = fst
+ofCoord One = snd
+ofCoord Two = trd
